0dbc24c1ed6cf281b8754cef891f50555c162968
Christopher Floyd <cfloyd252@gmail.com>
2020-04-07 13:42:05 -0700

    completed drill 5

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const BinarySearchTree = require('./BTS');
 
 function main() {
-  const BST = new BinarySearchTree()
+  const BST = new BinarySearchTree();
   BST.insert(3);
   BST.insert(1);
   BST.insert(4);
@@ -10,13 +10,13 @@ function main() {
   BST.insert(2);
   BST.insert(5);
   BST.insert(7);
-  return BST
+  return BST;
 }
 
 // console.log(main())
 
 function main2() {
-   const BST2 = new BinarySearchTree()
+   const BST2 = new BinarySearchTree();
    BST2.insert('E');
    BST2.insert('A');
    BST2.insert('S');
@@ -29,9 +29,9 @@ function main2() {
    BST2.insert('I');
    BST2.insert('O');
    BST2.insert('N');
-   return BST2
+   return BST2;
 }
-console.log(main2())
+// console.log(main2())
 
 //4 
 //Returns the total value of the tree (O(n))
@@ -44,11 +44,20 @@ console.log(main2())
 
 //5
 function findHeight(BST) {
-  let heightCount = 0
-  let currentLeftNode = BST.key
-  let currentRightNode = BST.key
-
-  while(currentLeftNode !== null || currentRightNode !== null) {
-    
+  if(BST.left === null && BST.right === null) {
+    return 1;
+  } else if (BST.left === null) {
+    return findHeight(BST.right) + 1;
+  } else if(BST.right === null) {
+    return findHeight(BST.left) + 1;
   }
+
+  return Math.max(findHeight(BST.left),findHeight(BST.right)) + 1
 }
+
+let bst = main();
+let tree = new BinarySearchTree()
+
+// console.log(bst)
+
+console.log(findHeight(bst))
